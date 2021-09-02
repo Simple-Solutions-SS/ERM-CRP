eff6039920271da58ddd60e44768ae4d9adaf595
Luis Cascante <luisalonzocascante@gmail.com>
2021-09-02 13:58:31 -0600

    PR de prueba

--- src/componentes/paginas/Asientos/index.tsx
+++ src/componentes/paginas/Asientos/index.tsx
@@ -2,6 +2,7 @@ import { AppLayout } from "../../organismos/AppLayout/AppLayout";
 import { TablaAsientos } from "../../organismos/TablaAsientos";
 
 const Asientos = () => {
+  console.log("Hola");
   return (
     <AppLayout>
       <h1>Asientos</h1>
